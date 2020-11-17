d4b797b9a7efbeeca6236ca0f4d81a3c208ef824
Sakthi <62131636+velsakthi562@users.noreply.github.com>
2020-11-17 18:22:38 +0530

    Master Update

---
+++ Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private MovingCube cubePrefab;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   
+    public void CubeSpawn()
+    {
+        var cube = Instantiate(cubePrefab);
+
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            MovingCube.CurrentCube.Stop();
+        }
+    }
+}
